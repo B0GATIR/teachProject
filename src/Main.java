67666890556e803191dc81b315800765d95d5f13
B0GATIR <yura.kabantsov@gmail.com>
2022-09-07 17:09:13 +0300

    Прошел семь глав. Закончил на странице 293.

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,68 @@
 //import javax.swing.JOptionPane;
 //import java.util.*;
-import Packages.ColoredFigure;
+//import Packages.ColoredFigure;
 
 //class Base{
-//    int number;
-//    String text;
+//    String name;
 //
-//    Base(int number, String text){
-//        this.number = number;
-//        this.text = text;
+//    Base(String name){
+//        this.name = name;
+//    }
+//    Base(Base obj){
+//        this.name = obj.name;
 //    }
 //}
 //
-//interface Alpha{
-//    default void printSome(int number, String text){
-//        System.out.println(number + " " + text);
-//    }
-//}
+//class First extends Base{
+//    int code;
 //
-//interface Bravo{
-//    default int getSum(int number){
-//        return number + 100;
+//    public void show(){
+//        System.out.println(name + " " + code);
 //    }
-//}
 //
-//class New extends Base implements Alpha, Bravo{
-//    public void printSome(){
-//        System.out.println(number + " " + text);
+//    First(String name, int code){
+//        super(name);
+//        this.code = code;
 //    }
-//    New(int number, String text){
-//        super(number, text);
+//    First(First obj){
+//        super(obj);
+//        code = obj.code;
 //    }
 //}
-class Base{
-    String name;
 
-    Base(String name){
-        this.name = name;
-    }
-    Base(Base obj){
-        this.name = obj.name;
-    }
-}
+class Binomial{
+    private int[] binoms;
 
-class First extends Base{
-    int code;
 
-    public void show(){
-        System.out.println(name + " " + code);
+    Binomial(int n){
+        binoms = new int[n + 1];
+        binoms[0] = 1;
+        for (int i = 0; i <= n; i++){
+            binoms[i] = binoms[i + 1] * (n - i + 1) / i;
+        }
     }
 
-    First(String name, int code){
-        super(name);
-        this.code = code;
-    }
-    First(First obj){
-        super(obj);
-        code = obj.code;
+    @Override
+    public String toString() {
+        String text = "| ";
+        for (int i = 0; i < binoms.length; i++){
+            text += binoms[i] + " ";
+        }
+        return text;
     }
 }
 public class Main {
     public static void main(String[] args) {
-        First A = new First("a", 1);
-        First B = new First(A);
-        A.code = 2;
-        A.name = "b";
-        B.show();
-        A.show();
+//        First A = new First("a", 1);
+//        First B = new First(A);
+//        A.code = 2;
+//        A.name = "b";
+//        B.show();
+//        A.show();
+
+        Binomial objA = new Binomial(10);
+        Binomial objB = new Binomial(100);
+        System.out.println(objA.toString());
+        System.out.println(objB.toString());
     }
 }

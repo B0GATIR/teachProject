2e233ff84d551706de978a17da7b029199be18be
yurak <yura.kabantsov@gmail.com>
2022-09-08 09:29:29 +0300

    Прошел семь глав. Остановился на главе 8, страница 307.

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,43 @@
 //import javax.swing.JOptionPane;
-//import java.util.*;
+import java.util.*;
 //import Packages.ColoredFigure;
 
-//class Base{
-//    String name;
-//
-//    Base(String name){
-//        this.name = name;
-//    }
-//    Base(Base obj){
-//        this.name = obj.name;
-//    }
-//}
-//
-//class First extends Base{
-//    int code;
-//
-//    public void show(){
-//        System.out.println(name + " " + code);
-//    }
-//
-//    First(String name, int code){
-//        super(name);
-//        this.code = code;
-//    }
-//    First(First obj){
-//        super(obj);
-//        code = obj.code;
-//    }
-//}
+class BankAccount{
+    class Person{
+        String name;
+        int startSum;
+        int time;
 
-class Binomial{
-    private int[] binoms;
+        public double getSum(){
+            double allSum = startSum;
+            for (int i = 1; i <= time; i++){
+                allSum *= (1 + rate/100);
+            }
+            return allSum;
+        }
 
-
-    Binomial(int n){
-        binoms = new int[n + 1];
-        binoms[0] = 1;
-        for (int i = 0; i <= n; i++){
-            binoms[i] = binoms[i + 1] * (n - i + 1) / i;
+        Person(String name, int startSum, int time){
+            this.name = name;
+            this.startSum = startSum;
+            this.time = time;
         }
     }
 
-    @Override
-    public String toString() {
-        String text = "| ";
-        for (int i = 0; i < binoms.length; i++){
-            text += binoms[i] + " ";
-        }
-        return text;
+    double rate;
+    Person fellow;
+
+    private Person createPerson(){
+        Scanner jIn = new Scanner(System.in);
+        return new Person(jIn.nextLine(), Integer.parseInt(jIn.nextLine()), Integer.parseInt(jIn.nextLine()));
+    }
+    BankAccount(int rate){
+        this.rate = rate;
+        fellow = createPerson();
     }
 }
 public class Main {
     public static void main(String[] args) {
-//        First A = new First("a", 1);
-//        First B = new First(A);
-//        A.code = 2;
-//        A.name = "b";
-//        B.show();
-//        A.show();
-
-        Binomial objA = new Binomial(10);
-        Binomial objB = new Binomial(100);
-        System.out.println(objA.toString());
-        System.out.println(objB.toString());
+        BankAccount bank = new BankAccount(8);
+        System.out.printf("All sum = %.3f\n", bank.fellow.getSum());
     }
 }

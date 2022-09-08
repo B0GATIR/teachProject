78111c39b148d243697b740619e07b5e1d3979e3
B0GATIR <yura.kabantsov@gmail.com>
2022-09-08 16:24:59 +0300

    Прошел семь глав. Закончил на странице 309. Анонимные классы.

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,32 @@ class BankAccount{
         fellow = createPerson();
     }
 }
+
+abstract class Base{
+    private String name;
+
+    void show(){
+        System.out.println("Object is: " + name);
+    }
+
+    abstract void hello();
+
+    Base(String name){
+        this.name = name;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        BankAccount bank = new BankAccount(8);
-        System.out.printf("All sum = %.3f\n", bank.fellow.getSum());
+//        BankAccount bank = new BankAccount(8);
+//        System.out.printf("All sum = %.3f\n", bank.fellow.getSum());
+
+        //Темы для повторения 4.1 в эл.жур 25 апреля
+
+        Base obj = new Base("Red") {
+            @Override
+            void hello() {
+                System.out.println("It is no identified class");
+            }
+        }
     }
 }

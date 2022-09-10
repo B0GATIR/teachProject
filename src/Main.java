728e23f4320d94211b6246f1822509ecbea0fb5a
yurak <yura.kabantsov@gmail.com>
2022-09-10 13:48:59 +0300

    Прошел восемь глав, страница 338.

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,101 @@
 //import javax.swing.JOptionPane;
+import java.nio.charset.Charset;
 import java.util.*;
 //import Packages.ColoredFigure;
 
-class BankAccount{
-    class Person{
-        String name;
-        int startSum;
-        int time;
+//class BankAccount{
+//    class Person{
+//        String name;
+//        int startSum;
+//        int time;
+//
+//        public double getSum(){
+//            double allSum = startSum;
+//            for (int i = 1; i <= time; i++){
+//                allSum *= (1 + rate/100);
+//            }
+//            return allSum;
+//        }
+//
+//        Person(String name, int startSum, int time){
+//            this.name = name;
+//            this.startSum = startSum;
+//            this.time = time;
+//        }
+//    }
+//
+//    double rate;
+//    Person fellow;
+//
+//    private Person createPerson(){
+//        Scanner jIn = new Scanner(System.in);
+//        return new Person(jIn.nextLine(), Integer.parseInt(jIn.nextLine()), Integer.parseInt(jIn.nextLine()));
+//    }
+//    BankAccount(int rate){
+//        this.rate = rate;
+//        fellow = createPerson();
+//    }
+//}
 
-        public double getSum(){
-            double allSum = startSum;
-            for (int i = 1; i <= time; i++){
-                allSum *= (1 + rate/100);
-            }
-            return allSum;
-        }
-
-        Person(String name, int startSum, int time){
-            this.name = name;
-            this.startSum = startSum;
-            this.time = time;
-        }
-    }
-
-    double rate;
-    Person fellow;
-
-    private Person createPerson(){
-        Scanner jIn = new Scanner(System.in);
-        return new Person(jIn.nextLine(), Integer.parseInt(jIn.nextLine()), Integer.parseInt(jIn.nextLine()));
-    }
-    BankAccount(int rate){
-        this.rate = rate;
-        fellow = createPerson();
-    }
-}
-
-class Based<X>{
-    private X sm;
-
-    Based(X sm){
-        set(sm);
-    }
-
-    void set(X sm){
-        this.sm = sm;
-    }
-
-    X get(){
-        return sm;
-    }
-}
-
-class Alpha<X, Y>{
-    Based<X> obj;
-    Y second;
-
-    Alpha(X first, Y second){
-        obj = new Based<>(first);
-        this.second = second;
-    }
+class Base{
+    String data;
 
     void show(){
-        System.out.println(obj.get() + " " + second);
+        System.out.println(data);
+    }
+
+    Base(String data){
+        this.data = data;
     }
 }
+
+class Alpha extends Base{
+    int number;
+
+    void show(){
+        super.show();
+        System.out.println("This " + number);
+    }
+
+    Alpha(String x, int number){
+        super(x);
+        this.number =  number;
+    }
+}
+
+class Bravo extends Base{
+    String string;
+
+    void show(){
+        super.show();
+        System.out.println("This " + string);
+    }
+
+    Bravo(String x, String string){
+        super(x);
+        this.string =  string;
+    }
+}
+
+class MyClass<X extends Base>{
+    X obj;
+
+    void show(){
+        System.out.println("MyClass object");
+        obj.show();
+    }
+
+    MyClass(X obj){
+        this.obj = obj;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
 //        BankAccount bank = new BankAccount(8);
 //        System.out.printf("All sum = %.3f\n", bank.fellow.getSum());
-
-        //Темы для повторения 4.1 в эл.жур 25 апреля
-
-        Alpha<Integer, Character> A = new Alpha<>(100, 'A');
-        A.show();
+        MyClass<Alpha> obj = new MyClass<>(new Alpha("Alpha", 1));
+        obj.show();
     }
 }

d426044f9d5c7778260f23844990ecf77e4328c3
yurak <yura.kabantsov@gmail.com>
2022-09-17 20:14:39 +0300

    Прошел десять глав, остановился на перехвате и обработке ошибок, страница 393.

--- src/Main.java
+++ src/Main.java
@@ -2,70 +2,32 @@ import Packages.MathForProgramming;
 import Packages.LeetCode;
 import java.util.*;
 
-
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
-
 public class Main {
+    public static boolean canBeConstruct(String ransomNote, String magazine){
+        char[] ransomNoteArray = ransomNote.toCharArray();
+        char[] magazineArray = magazine.toCharArray();
 
-    //Не входит в программу LeetCode
-    static ListNode getListNode(int length){
-        Scanner jIn = new Scanner(System.in);
-
-        ListNode head = new ListNode(Integer.parseInt(jIn.nextLine()));
-        ListNode end = head;
-
-        for (int i = 1; i < length; i++){
-            end.next = new ListNode(Integer.parseInt(jIn.nextLine()));
-            end = end.next;
-        }
-
-        end.next = null;
-
-        return head;
-    }
-    //Входит в LeetCode
-    static ArrayList<Integer> convertListNodeInArrayList(ListNode head){
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        ListNode end = head;
-
-        do {
-            arrayList.add(end.val);
-            end = end.next;
-        } while (end != null);
-
-        return arrayList;
-    }
-
-    static boolean isPalindrome(ArrayList<Integer> arrayList){
-
-        boolean isIt = true;
-
-        for (int i = 0; i < arrayList.size()/2; i++){
-            if (arrayList.get(i) == arrayList.get((arrayList.size() - 1) - i)){
-                isIt = true;
-            } else {
-                return false;
+        int counter = 0;
+        for (int i = 0; i < ransomNoteArray.length; i++){
+            for (int j = 0; j < magazineArray.length; j++){
+                if (ransomNoteArray[i] == magazineArray[j]){
+                    counter++;
+                    magazineArray[j] = ' ';
+                    System.out.println(magazine);
+                    break;
+                }
             }
         }
 
-        return isIt;
+
+        if (counter == ransomNote.length())
+            return true;
+        else
+            return false;
     }
 
+
     public static void main(String[] args) {
-        int length = 4;
-        ArrayList<Integer> arrayList = convertListNodeInArrayList(getListNode(length));
-
-        for (int i = 0; i < arrayList.size(); i++){
-            System.out.println(arrayList.get(i));
-        }
-
-        System.out.println(isPalindrome(arrayList));
+        System.out.println(canBeConstruct("aa", "ab"));
     }
 }
--- src/Packages/LeetCode.java
+++ src/Packages/LeetCode.java
@@ -2,64 +2,17 @@ package Packages;
 
 import java.util.*;
 
-class RomanNumeral{
-    char sym;
-    int num;
-
-    static public int findNum(RomanNumeral[] array, char s){
-        int result = 0;
-        for (RomanNumeral obj: array) {
-            if (obj.sym == s){
-                result =  obj.num;
-            }
-        }
-        return result;
-    }
-
-    static RomanNumeral[] getAllRomanNumerals(){
-        RomanNumeral[] array = new RomanNumeral[7];
-
-        array[0] = new RomanNumeral('I', 1);
-        array[1] = new RomanNumeral('V', 5);
-        array[2] = new RomanNumeral('X', 10);
-        array[3] = new RomanNumeral('L', 50);
-        array[4] = new RomanNumeral('C', 100);
-        array[5] = new RomanNumeral('D', 500);
-        array[6] = new RomanNumeral('M', 1000);
-
-        return array;
-    }
-
-    static int getArabianNum(RomanNumeral[] array, char[] string){
-        int result = 0;
-        for (int i = 0; i < string.length - 1; i++){
-            if (RomanNumeral.findNum(array, string[i]) <
-                    RomanNumeral.findNum(array, string[i + 1])){
-                result += RomanNumeral.findNum(array, string[i]) * -1;
-            } else {
-                result += RomanNumeral.findNum(array, string[i]);
-            }
-        }
-        return result  + RomanNumeral.findNum(array, string[string.length - 1]);
-    }
-
-    RomanNumeral(char sym, int num){
-        this.num = num;
-        this.sym = sym;
-    }
-
-    @Override
-    public String toString(){
-        return sym + " " + num;
-    }
-}
+//    Реверс связанной цепи
+//    static ListNode reverse(ListNode head){
+//        ListNode reverse = new ListNode(-1);
+//        while (head != null){
+//            ListNode next = head.next;
+//            head.next = reverse.next;
+//            reverse.next = head;
+//            head = next;
+//        }
+//        return reverse.next;
+//    }
 public class LeetCode {
-    static public void RomanInInteger(){
-        RomanNumeral[] arrayRomanNumerals = RomanNumeral.getAllRomanNumerals();
 
-        Scanner jIn = new Scanner(System.in);
-        char[] string = jIn.nextLine().toCharArray();
-
-        System.out.println(RomanNumeral.getArabianNum(arrayRomanNumerals, string));
-    }
 }

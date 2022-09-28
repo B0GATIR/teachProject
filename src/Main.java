ae04f795f52b72d0fe947365c186703da7be8368
yurak <yura.kabantsov@gmail.com>
2022-09-28 09:50:08 +0300

    Прошел десять глав, остановился на контролируемых и неконтролируемых исключениях, страница 420.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
 import java.text.ParseException;
 import java.util.*;
 
-
 public class Main {
 
-    static public int toZero(int step, int num){
-        if (num == 0){
-            return step;
-        } else if (num % 2 == 0){
-            step++;
-            return toZero(step, num / 2);
-        } else {
-            step++;
-            return toZero(step, num - 1);
+    static public int[] getArray(int length){
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++){
+            array[i] = i;
         }
+
+        return array;
     }
+
     public static void main(String[] args) {
+        Exception me = new Exception("me Error");
         Scanner jIn = new Scanner(System.in);
 
-        int input = Integer.parseInt(jIn.nextLine());
-
-        System.out.println(toZero(0, input));
+        try {
+            int l = jIn.nextInt();
+            int[] array = getArray(l);
+            try {
+                throw me;
+//                int i = jIn.nextInt();
+//                System.out.println(array[i]);
+            } catch (Exception e){
+                System.out.println(e);
+            } finally {
+                for (int i : array) {
+                    System.out.print(i + " ");
+                }
+            }
+        } catch (NegativeArraySizeException e){
+            System.out.println(e);
+        } catch(InputMismatchException e){
+            System.out.println(e);
+        }
     }
 }
